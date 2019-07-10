946d01effb38df10d9ee9351b3cbe90e395480a8
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-07-10 17:09:03 +0100

    implementation express server with routes moved from server to index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code away!
+import express from 'express';
+
+import userRoutes from './api/users/userRouter';
+import postsRoutes from './api/posts/postRouter';
+
+const server = express();
+server.use(express.json());
+
+server.use('/api', userRoutes);
+server.use('/api', postsRoutes);
+
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Lambda BLOG API</h>
+    <p>Welcome to the Lambda BLOG API</p>
+  `);
+});
+
+server.listen(7000, () => {
+  console.log(`\n*** Server Running on 7000 ***\n`);
+});
