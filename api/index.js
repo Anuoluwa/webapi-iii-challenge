407fcbe8d9615e248c24f63cfb413cead747b242
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-07-10 17:05:09 +0100

    restructure folder for both users and posts

---
+++ api/index.js
@@ -0,0 +1,13 @@
+// import express from 'express';
+
+
+import {userRoutes} from './users/userRouter';
+import {postsRoutes} from './posts/postRouter';
+const route = {userRoutes, postsRoutes}
+export default route;
+// const route = express.Router();
+
+
+
+
+
