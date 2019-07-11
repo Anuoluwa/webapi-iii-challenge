cf146b7e94c9ec684ea7e9a699099c8c89eb4329
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-07-11 13:01:01 +0100

    add middleswares as logger to console.log all required specs

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,3 +1,11 @@
-const logger = (req, res) => {
-    console.log(req.body, req.method, date.now())
-}
+const logger = (req, res, next) => {
+  let currenttime = Date();
+  let timestamp = currenttime.toString();
+  console.log(`These are the details of the your requests =>
+    X-REQ URL:${req.url},
+    X-REQ METHOD:${req.method}, 
+    X-TIMESTAMP: ${timestamp}`);
+  next();
+};
+
+export default logger;
