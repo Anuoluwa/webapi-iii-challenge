9c2ec318f5c464996e3af2f2205413a8d62621cf
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-07-11 13:00:23 +0100

    add middleswares to validate id across all routes

--- middlewares/validateId.js
+++ middlewares/validateId.js
@@ -1,10 +1,10 @@
-function validateId(req, res, next) {
+export default function validateId(req, res, next) {
     const { id } = req.params;
     const parsedId = parseInt(id, 10);
     if (id.length === '') {
       return res.status(400).json({ error: '"ID" should not be empty!' });
     }
-    if (Number.isNaN(parsedId) === true) {
+    if (isNaN(parsedId) === true) {
       return res.status(400).json({
         status: 409,
         error: 'Id must be a number',
